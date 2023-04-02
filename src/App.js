9c5f8db769ba00989ca58cf2dc8d7c250f083db1
Cbravor1991 <cbravor@fi.uba.ar>
2023-04-02 03:06:41 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import HomePage from './routes/HomePage';
+import DashBoardPage from './routes/DashboardPage';
+
+import { Routes, Route } from 'react-router-dom'
+
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [registrationSuccessful, setRegistrationSuccessful] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          {/* public routes */}
+         
+          <Route path="dashboard" element={<DashBoardPage />} />
+         
+         
+
+          {/*
+        <Route element={<RequireAuth allowedRoles={[ROLES.Inquilino]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+  
+        <Route element={<RequireAuth allowedRoles={[ROLES.Casero]} />}>
+          <Route path="editor" element={<Editor />} />
+        </Route>
+  
+  
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin" element={<Admin />} />
+        </Route>
+  
+        <Route element={<RequireAuth allowedRoles={[ROLES.Casero, ROLES.Admin]} />}>
+          <Route path="lounge" element={<Lounge />} />
+        </Route> */}
+
+
+         
+        </Route>
+      </Routes>
+    </>
   );
 }
 
