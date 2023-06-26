3ba92d5382898e9e93f352d48705a1fc2a8212ee
Cbravor1991 <cbravor@fi.uba.ar>
2023-06-26 02:10:50 -0300

    configuracion deploy

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -13,13 +13,56 @@ import ShowsOrganizer from '../components/showOrganizer';
 import ShowsTopOrganizerEvent from '../components/showOrganizerTopEvent'
 import ShowsTopOrganizerAcreditation from '../components/showOrganizerTopAcreditation'
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TextField from '@mui/material/TextField';
+
+
+import { Link } from 'react-router-dom';
+
 require('highcharts/modules/map')(Highcharts);
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#1286f7',
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+
+
+
+
+
+
+
+
 
 
 const Dashboard = () => {
   const [initialDate, setInitialDate] = useState(dayjs("2023-04-01"));
   let today = moment().format('yyyy-MM-DD');
-  const [finalDate, setFinalDate] = useState(dayjs(today ));
+  const [finalDate, setFinalDate] = useState(dayjs(today));
   const [unit, setUnit] = useState('day');
   const [dataState, setDataState] = useState([]);
   const [dataAttendances, setDataAttendances] = useState([]);
@@ -41,6 +84,8 @@ const Dashboard = () => {
   const [dataTopRanking, setDataTopRanking] = useState([]);
   const [emptyDataTopRanking, setEmptyDataTopRanking] = useState(false);
 
+
+
   const translate = {
     "published": "publicado",
     "draft": "borrador",
@@ -52,8 +97,9 @@ const Dashboard = () => {
   console.log(window.localStorage.getItem("token"));
 
   const fetchDataRankingOption2 = async () => {
-   
+
   };
+
 
 
   const verifyMonth = async (month) => {
@@ -83,9 +129,9 @@ const Dashboard = () => {
 
   const chargeDate = async () => {
     const months = [
-         'jan', 'feb', 'mar', 'apr', 'may', 'jun',
-         'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
-     ];
+      'jan', 'feb', 'mar', 'apr', 'may', 'jun',
+      'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
+    ];
 
     const initialMonth = parseInt(initialDate.format('MM'));
     const initialYear = parseInt(initialDate.format('YYYY'));
@@ -120,7 +166,7 @@ const Dashboard = () => {
 
 
   const fetchDataTopAcreditation = async () => {
-  
+
     const load = [];
     let token_user;
     if (!window.localStorage.getItem("token")) {
@@ -171,7 +217,8 @@ const Dashboard = () => {
         await Promise.all(promises); // Esperar a que se completen todas las promesas 
 
         setDataTopAcreditations(load);
-        
+
+
       } else { setDataTopAcreditations([]) }
 
     } catch (error) {
@@ -258,15 +305,15 @@ const Dashboard = () => {
     try {
 
       const response = await axios(options);
-     
+
       if (response.data) {
 
         setDataState([]);
 
-        for(let i in response.data){
-          setDataState(dataState => [...dataState, ({"name":translate[i],"y":response.data[i]})]);
+        for (let i in response.data) {
+          setDataState(dataState => [...dataState, ({ "name": translate[i], "y": response.data[i] })]);
         }
-        
+
       } else {
         setDataState([])
       }
@@ -299,14 +346,14 @@ const Dashboard = () => {
 
         if (response.data) {
           console.log(response.data);
-          
+
           for (let i in dateCategories) {
             data_Attendances.push(0)
           }
           for (let i in response.data) {
-            if (await verifyMonth(response.data[i].date.substring(0,3)) >= 0) {
-              let position = await verifyMonth(response.data[i].date.substring(0,3));
-  
+            if (await verifyMonth(response.data[i].date.substring(0, 3)) >= 0) {
+              let position = await verifyMonth(response.data[i].date.substring(0, 3));
+
               data_Attendances[position] = data_Attendances[position] + response.data[i].attendances
 
             }
@@ -326,7 +373,7 @@ const Dashboard = () => {
 
   const fetchDataEvents = async () => {
     //const finalDate = dayjs(finalDate).add(1, 'day');
-    
+
     const data_Events = []
     var options = {
       method: 'GET',
@@ -352,8 +399,8 @@ const Dashboard = () => {
           }
 
           for (let i in response.data) {
-            if (await verifyMonth(response.data[i].date.substring(0,3)) >= 0) {
-              let position = await verifyMonth(response.data[i].date.substring(0,3));
+            if (await verifyMonth(response.data[i].date.substring(0, 3)) >= 0) {
+              let position = await verifyMonth(response.data[i].date.substring(0, 3));
 
               data_Events[position] = data_Events[position] + response.data[i].events;
 
@@ -397,8 +444,8 @@ const Dashboard = () => {
             data_Complaints.push(0)
           }
           for (let i in response.data) {
-            if (await verifyMonth(response.data[i].date.substring(0,3)) >= 0) {
-              let position = await verifyMonth(response.data[i].date.substring(0,3));
+            if (await verifyMonth(response.data[i].date.substring(0, 3)) >= 0) {
+              let position = await verifyMonth(response.data[i].date.substring(0, 3));
 
               data_Complaints[position] = data_Complaints[position] + response.data[i].complaints
 
@@ -442,11 +489,11 @@ const Dashboard = () => {
             data_Suspension.push(0)
           }
           for (let i in response.data) {
-            if (await verifyMonth(response.data[i].date.substring(0,3)) >= 0) {
-              let position = await verifyMonth(response.data[i].date.substring(0,3));
+            if (await verifyMonth(response.data[i].date.substring(0, 3)) >= 0) {
+              let position = await verifyMonth(response.data[i].date.substring(0, 3));
 
               data_Suspension[position] = data_Suspension[position] + response.data[i].suspensions
-             
+
 
             }
           }
@@ -492,8 +539,56 @@ const Dashboard = () => {
     fetchDataComplaints();
     fetchDataSuspension();
 
+
+
     window.addEventListener('resize', handleResize);
   }, [initialDate, finalDate]);
+
+  const [searchText, setSearchText] = React.useState('');
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [sortingColumn, setSortingColumn] = useState('events');
+  const [sortingDirection, setSortingDirection] = useState('desc');
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSortingColumnChange = (event) => {
+    setSortingColumn(event.target.value);
+  };
+
+  const handleRowsPerPageChange = (event) => {
+    setRowsPerPage(event.target.value);
+  };
+
+  const sortedData = [...dataTopAcreditations];
+  sortedData.sort((a, b) => {
+    if (sortingColumn === 'people') {
+      return sortingDirection === 'desc' ? a.attendances - b.attendances : b.attendances - a.attendances;
+    } else if (sortingColumn === 'events') {
+      return sortingDirection === 'asc' ? a.attendances - b.attendances : b.attendances - a.attendances;
+    }
+    return 0;
+  });
+
+  const filteredData = sortedData.filter((row) =>
+    row.organizer_email.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  const sortedData_ = [...dataTopEvents];
+  sortedData_.sort((a, b) => {
+    if (sortingColumn === 'events') {
+      return sortingDirection === 'asc' ? a.amount - b.amount : b.amount - a.amount;
+    } else if (sortingColumn === 'people') {
+      return sortingDirection === 'desc' ? a.amount - b.amount : b.amount - a.amount;
+    }
+    return 0;
+  });
+
+  const filteredData_ = sortedData_.filter((row) =>
+    row.organizer_email.toLowerCase().includes(searchText.toLowerCase())
+  );
+
 
   return (
     <div>
@@ -667,20 +762,209 @@ const Dashboard = () => {
 
           <Grid item xs={6} s>
             <Paper style={{ padding: "5px", color: 'grey' }} elevation={3}>
-            
-              {dataTopEvents.length > 0 && !emptyDataTopEvents && <ShowsTopOrganizerEvent dataTopEvents={dataTopEvents} />}
-              {dataTopEvents.length == 0 && <ShowsTopOrganizerEvent dataTopEvents={dataTopEvents} />}
+              {console.log('los top eventos', dataTopEvents)}
+              
+              <Typography variant="h6" component="div" sx={{ marginLeft: '160px', color: 'black', fontSize: 24, fontWeight: 700, mb: 2, marginTop: '20px' }}>
+      Ranking organizadores por eventos
+      </Typography>
+      <Grid sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Paper sx={{ width: '100%' }} elevation={5}>
+          <TableContainer component={Grid}>
+            <div>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-label">Mostrar</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={rowsPerPage}
+                  label="Mostrar"
+                  onChange={handleRowsPerPageChange}
+                >
+                  <MenuItem sx={{ color: 'black' }} value={5}>5</MenuItem>
+                  <MenuItem sx={{ color: 'black' }} value={10}>10</MenuItem>
+                  <MenuItem sx={{ color: 'black' }} value={25}>25</MenuItem>
+                </Select>
+              </FormControl>
+
+              <TextField
+                sx={{ m: 1, width: '30ch' }}
+                label="Buscar por organizador"
+                variant="outlined"
+                value={searchText}
+                onChange={handleSearchChange}
+              />
+
+               <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-label">Ordenar por</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={sortingColumn}
+          label="Ordenar por"
+          onChange={handleSortingColumnChange}
+        >
+          <MenuItem sx={{ color: 'black' }} value={'events'}>Mayor cantidad de eventos</MenuItem>
+          <MenuItem sx={{ color: 'black' }} value={'people'}>Menor cantidad de eventos</MenuItem>
+        </Select>
+      </FormControl>
+    
+
+            </div>
+
+            <Grid sx={{ maxHeight: '700px', overflowY: 'scroll' }}>
+              <Table sx={{ minWidth: 700 }} aria-label="customized table">
+
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell align="center"> Mail</StyledTableCell>
+                    <StyledTableCell align="center">Cantidad de eventos</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+
+                {(dataTopEvents && dataTopEvents.length > 0) ?
+                  (
+                    <TableBody>
+                      {filteredData_
+                        .slice(0, rowsPerPage)
+                        .map((row) => (
+                          <StyledTableRow key={row.organizer_email}>
+                            <StyledTableCell align="center" component="th" scope="row">
+                              {row.organizer_email}
+                            </StyledTableCell>
+                            <StyledTableCell align="center">
+                              {row.amount}
+                            </StyledTableCell>
+                          </StyledTableRow>
+                        ))
+                      }
+                    </TableBody>
+                  )
+                  :
+                  (
+                    <StyledTableRow >
+                      <StyledTableCell component="th" scope="row">
+                        NO HAY ORGANIZADORES
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  )}
+              </Table>
+            </Grid>
+
+          </TableContainer>
+        </Paper>
+      </Grid>
+
+
+
+
+
+
+
+
 
 
 
             </Paper>
           </Grid>
+          {console.log('ver esto=>', dataTopAcreditations)}
 
           <Grid item xs={6} s>
             <Paper style={{ padding: "5px", color: 'grey' }} elevation={3}>
-              {console.log('acreditaciones=>',dataTopAcreditations.length)}
-              {dataTopAcreditations.length > 0 && <ShowsTopOrganizerAcreditation dataTopAcreditations={dataTopAcreditations} />}
-              {dataTopAcreditations.length == 0 && <ShowsTopOrganizerAcreditation dataTopAcreditations={dataTopAcreditations} />}
+
+              <Typography variant="h6" component="div" sx={{ marginLeft: '160px', color: 'black', fontSize: 24, fontWeight: 700, mb: 2, marginTop: '20px' }}>
+                Ranking organizadores por acreditaciones
+              </Typography>
+              <Grid sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Paper sx={{ width: '100%' }} elevation={5}>
+                  <TableContainer component={Grid}>
+                    <div>
+                      <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-label">Mostrar</InputLabel>
+                        <Select
+                          labelId="demo-simple-select-label"
+                          id="demo-simple-select"
+                          value={rowsPerPage}
+                          label="Mostrar"
+                          onChange={handleRowsPerPageChange}
+                        >
+                          <MenuItem sx={{ color: 'black' }} value={5}>5</MenuItem>
+                          <MenuItem sx={{ color: 'black' }} value={10}>10</MenuItem>
+                          <MenuItem sx={{ color: 'black' }} value={25}>25</MenuItem>
+                        </Select>
+                      </FormControl>
+
+                      <TextField
+                        sx={{ m: 1, width: '30ch' }}
+                        label="Buscar por organizador"
+                        variant="outlined"
+                        value={searchText}
+                        onChange={handleSearchChange}
+                      />
+
+                      <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-label">Ordenar por</InputLabel>
+                        <Select
+                          labelId="demo-simple-select-label"
+                          id="demo-simple-select"
+                          value={sortingColumn}
+                          label="Ordenar por"
+                          onChange={handleSortingColumnChange}
+                        >
+                          <MenuItem sx={{ color: 'black' }} value={'events'}>Mayor cantidad de acreditaciones</MenuItem>
+                          <MenuItem sx={{ color: 'black' }} value={'people'}>Menor cantidad de acreditaciones</MenuItem>
+                        </Select>
+                      </FormControl>
+
+
+                    </div>
+
+                    <Grid sx={{ maxHeight: '700px', overflowY: 'scroll' }}>
+                      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+
+                        <TableHead>
+                          <TableRow>
+                            <StyledTableCell align="center"> Mail</StyledTableCell>
+                            <StyledTableCell align="center">Cantidad de acreditaciones</StyledTableCell>
+                          </TableRow>
+                        </TableHead>
+
+                        {(dataTopAcreditations && dataTopAcreditations.length > 0) ?
+                          (
+                            <TableBody>
+                              {filteredData
+                                .slice(0, rowsPerPage)
+                                .map((row) => (
+                                  <StyledTableRow key={row.organizer_email}>
+                                    <StyledTableCell align="center" component="th" scope="row">
+                                      {row.organizer_email}
+                                    </StyledTableCell>
+                                    <StyledTableCell align="center">
+                                      {row.attendances}
+                                    </StyledTableCell>
+                                  </StyledTableRow>
+                                ))
+                              }
+                            </TableBody>
+                          )
+                          :
+                          (
+                            <StyledTableRow >
+                              <StyledTableCell component="th" scope="row">
+                                NO HAY ORGANIZADORES
+                              </StyledTableCell>
+                            </StyledTableRow>
+                          )}
+                      </Table>
+                    </Grid>
+
+                  </TableContainer>
+                </Paper>
+              </Grid>
+
+
+
+
+
 
 
 
